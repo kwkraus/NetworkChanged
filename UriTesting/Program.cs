d4f26666b4f8f8d1854bcb0957adcefe0b830cf2
Kevin Kraus <kevin.kraus@outlook.com>
2016-11-30 08:55:22 -0700

    initial

---
+++ UriTesting/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace UriTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "localhost:5050";
+            host = "dav.dev.uspto.gov";
+
+            System.Net.NetworkInformation.NetworkChange.NetworkAddressChanged += new
+
+            System.Net.NetworkInformation.NetworkAddressChangedEventHandler(AddressChangedCallback);
+            Console.WriteLine("Listening for address changes. Press any key to exit.");
+            Console.ReadLine();
+            bool connected = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+
+            if (connected)
+            {
+                Uri validUri = null;
+                bool isValid = Uri.TryCreate(host, UriKind.RelativeOrAbsolute, out validUri);
+
+                UriBuilder builder = new UriBuilder(host);
+
+                Console.WriteLine(validUri.ToString());
+                Console.WriteLine(builder.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Not connected");
+            }
+            Console.Read();
+        }
+
+        static void AddressChangedCallback(object sender, EventArgs e)
+        {
+
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface n in adapters)
+            {
+                Console.WriteLine("   {0} is {1}", n.Name, n.OperationalStatus);
+            }
+        }
+    }
+}
